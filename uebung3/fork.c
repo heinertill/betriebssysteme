271b59a7c88c579c80972af1113cf5f0e3c4f830
fdai6978 <till.heiner@informatik.hs-fulda.de>
2023-05-05 12:36:28 +0200

    changes to fork

--- uebung3/fork.c
+++ uebung3/fork.c
@@ -1,17 +1,27 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <unistd.h>
+int a_global = 10;
 
 int main(int argc, char **argv) {
 	int pid;
+	int a_priv = 10;
+
+	printf("global address before fork: %p - local address before fork: %p\n", &a_global, &a_priv);
+
 	printf("Hauptprozess - SID: %d, PID: %d, PPID: %d\n", getsid(0), getpid(), getppid());
+
+	printf("\n--- Creating forks --- \n\n");
 
 	pid = fork();
 
+
 	if (pid > 0) {
 		printf("Hallo vom Elternprozess - PID: %d, Kind-ID: %d\n", getpid(), pid);
+		printf("PARENT: global address after fork: %p - local address after fork: %p\n", &a_global, &a_priv);
 	} else if (pid == 0) {
 		printf("Hallo vom Kindprozess - PID: %d, PPID: %d\n", getpid(), getppid()); 
+		printf("CHILD: global address after fork: %p - local address after fork: %p\n", &a_global, &a_priv);
 	} else {
 		printf("Fork Fehler aufgetreten.");
 		exit(EXIT_FAILURE);
